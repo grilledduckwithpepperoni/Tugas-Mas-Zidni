a3e12e38054c75ab6084bc492b9df78c3d9a3fac
grilledduckwithpepperoni <80267014+grilledduckwithpepperoni@users.noreply.github.com>
2021-03-09 14:28:20 +0700

    Add files via upload

---
+++ Struk.py
@@ -0,0 +1,15 @@
+print("------------------------------")
+print("    Sewa Mobil UNEJRentCar    ")
+print("    Jl Kalimantan 37 Jember   ")
+print("------------------------------")
+print("")
+
+durasisewa = int(input("Lama Sewa = "))
+sewaperhari = int(input("Biaya Sewa Perhari = Rp. "))
+total = durasisewa * sewaperhari
+print("Total Biaya = Rp. "+str(total))
+print("")
+
+print("------------------------------")
+print("Terima kasih telah menggunakan")
+print("          jasa kami           ")  
